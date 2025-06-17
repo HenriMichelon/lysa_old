4d51f74c7abcbfb72e824748c1e2fb08b329cfd7
HenriMichelon <148531229+HenriMichelon@users.noreply.github.com>
2025-06-17 12:53:32 +0200

    PhysX MeshShape

--- src/lysa/physics/physx/MeshShape.cpp
+++ src/lysa/physics/physx/MeshShape.cpp
@@ -8,24 +8,43 @@ module;
 #include <PxPhysicsAPI.h>
 module lysa.resources.mesh_shape;
 
+import lysa.global;
+
 namespace lysa {
 
     void MeshShape::createShape(const std::shared_ptr<MeshInstance>& meshInstance) {
         const auto & vertices  = meshInstance->getMesh()->getVertices();
-        // JPH::VertexList vertexList;
-        // vertexList.reserve(vertices.size());
-        // for (const auto &vertex : vertices) {
-        //     vertexList.push_back(JPH::Float3{vertex.position.x, vertex.position.y, vertex.position.z});
-        // }
-        //
-        // const auto & indices = meshInstance->getMesh()->getIndices();
-        // JPH::IndexedTriangleList triangles;
-        // triangles.reserve(indices.size()/3);
-        // for (int i = 0; i < indices.size(); i += 3) {
-        //     triangles.push_back({indices[i + 0], indices[i + 1], indices[i + 2]});
-        // }
-        //
-        // shapeHandle = new JPH::MeshShapeSettings(vertexList, triangles);
+        const auto& indices = meshInstance->getMesh()->getIndices();
+
+        std::vector<physx::PxVec3> pxVertices;
+        pxVertices.reserve(vertices.size());
+        for (const auto& v : vertices) {
+            pxVertices.emplace_back(v.position.x, v.position.y, v.position.z);
+        }
+
+        std::vector<physx::PxU32> pxIndices;
+        pxIndices.reserve(indices.size());
+        for (uint32_t index : indices) {
+            pxIndices.push_back(index);
+        }
+
+        auto meshDesc = physx::PxTriangleMeshDesc {};
+        meshDesc.points.count     = static_cast<physx::PxU32>(pxVertices.size());
+        meshDesc.points.stride    = sizeof(physx::PxVec3);
+        meshDesc.points.data      = pxVertices.data();
+        meshDesc.triangles.count  = static_cast<physx::PxU32>(pxIndices.size() / 3);
+        meshDesc.triangles.stride = 3 * sizeof(physx::PxU32);
+        meshDesc.triangles.data   = pxIndices.data();
+        meshDesc.flags            = physx::PxMeshFlag::e16_BIT_INDICES;
+
+        const physx::PxCookingParams cookingParams{physx::PxTolerancesScale{}};
+        physx::PxDefaultMemoryOutputStream stream;
+        if (!PxCookTriangleMesh(cookingParams, meshDesc, stream)) {
+            throw Exception("MeshShape Failed to cook triangle mesh");
+            return;
+        }
+        physx::PxDefaultMemoryInputData input(stream.getData(), stream.getSize());
+        geometry = std::make_unique<physx::PxTriangleMeshGeometry>(getPhysx()->createTriangleMesh(input));
     }
 
 }
